3715964fd585a337694eb01b6daf06b841a243d2
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-20 12:27:14 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
       resolve: 'gatsby-source-wordpress',
       options: {
         // The base url to your WP site.
-        baseUrl: 'gatsbynetliflydemo.justinwhall.com',
+        baseUrl: 'surfbnb.tv/restapi/',
         // baseUrl: 'wpgatsby.wtf',
         // WP.com sites set to true, WP.org set to false
         hostingWPCOM: false,
