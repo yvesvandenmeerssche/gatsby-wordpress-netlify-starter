6d4dba5f198f424e8541b333db8366bd03ab06da
Yves Van den Meerssche <45092543+yvesvandenmeerssche@users.noreply.github.com>
2018-12-20 20:59:39 +0100

    Update SiteConfig.js

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,17 +2,17 @@ module.exports = {
   siteTitle: "Gatsby + WordPress + Netlifly Starter", // Site title.
   siteTitleAlt: "GatsbyJS WordPress Netlifly API Starter", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "http://wordpress-154986-534401.cloudwaysapps.com", // Domain of your website without pathPrefix.
+  siteUrl: "http://wordpressproduction.netlify.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "A GatsbyJS + Netlifly starter with the WordPress API built in mind.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
   siteFBAppID: "", // FB Application ID for using app insights
   googleAnalyticsID: "", // GA tracking ID.
   disqusShortname: "", // Disqus shortname.
-  postDefaultCategoryID: "Tech", // Default category for posts.
+  postDefaultCategoryID: "Uncategorized", // Default category for posts.
   userName: "User", // Username to display in the author segment.
-  userTwitter: "@justinwhall", // Optionally renders "Follow Me" in the UserInfo segment.
-  userLocation: "Denver, CO, USA ", // User location to display in the author segment.
+  userTwitter: "@livethelifetv", // Optionally renders "Follow Me" in the UserInfo segment.
+  userLocation: "Jeffreys Bay, South Africa ", // User location to display in the author segment.
   userAvatar: "https://api.adorable.io/avatars/150/test.png", // User avatar to display in the author segment.
   userDescription:
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
